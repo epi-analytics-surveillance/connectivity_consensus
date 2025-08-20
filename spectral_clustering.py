7e571a10fdd64b0ef465212de0d839d238869b9e
rccreswell <56924958+rccreswell@users.noreply.github.com>
2025-08-20 23:22:25 +1000

    Update spectral_clustering.py

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -14,7 +14,9 @@ assign_chain = [[[1, 1, 2], [1, 1, 2], [1, 2, 3],],]
 
 
 def compute_connectivity_matrix(assign_chain, n):
-    """Compute an n x n matrix, where entry (i, j) is the number of times data point i and data point j were in the same cluster.
+    """Compute an n x n matrix, where entry (i, j)
+    is the number of MCMC samples in which data
+    point i and data point j were in the same cluster.
     """
     connectivity = np.zeros((n, n))
 
@@ -30,11 +32,17 @@ def compute_connectivity_matrix(assign_chain, n):
 
     
 def consensus_clustering(num_clusters_chain, assign_chain, n):
+    """Compute a single consensus clustering based on the mode number of clusters
+    in the MCMC chain and the number of MCMC samples where data points co-clustered 
+    with each other.
+    """
     num_clusters_samples = [x for sublist in num_clusters_chain for x in sublist]
     num_clusters = scipy.stats.mode(num_clusters_samples)[0]
     
     connectivity = compute_connectivity_matrix(assign_chain, n)
-    
+
+    # connectivity: matrix where larger values indicate data points which are closer together.
+    # Probably could also use probability of co-clustering across the MCMC chain
     m = SpectralClustering(n_clusters=num_clusters, affinity='precomputed').fit(connectivity)
     
     return num_clusters, m.labels_
